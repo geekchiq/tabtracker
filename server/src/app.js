b7e97a5ab2cf66b5d779cc92fef3884a9830b07b
javachiq <thegeekchiq@gmail.com>
2022-07-13 21:03:34 +0800

    Update - DB changed to SQLLite

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const config = require('./config/config')
-// const { sequelize } = require('./models')
+const { sequelize, Song } = require('./models')
 
 const app = express()
 app.use(morgan('combined'))
@@ -16,19 +16,26 @@ app.use(
 )
 app.use(cors())
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-// app.post('/', function (req, res) {
-//   res.send('Got a POST request')
-// })
 require('./routes')(app)
 
-app.listen(config.port)
-console.log(`Server started at ${config.port}`)
+// app.listen(config.port)
+// console.log(`Server started at ${config.port}`)
 
-// sequelize.sync({ force: false })
-//   .then(() => {
-//     app.listen(config.port)
-//     console.log(`Server started at port ${config.port}`)
-//   })
+sequelize.sync({ force: false }).then(() => {
+  app.listen(config.port)
+  initial()
+  console.log(`Server started at port ${config.port}`)
+})
+
+function initial() {
+  Song.create({
+    title: 'Left And Right',
+    artist: 'Charlie Puth feat BTS',
+    genre: 'Pop',
+    album: 'Left And Right EP',
+    albumImageUrl: 'To Follow',
+    youtubeId: 'https://www.youtube.com/watch?v=a7GITgqwDVg',
+    lyrics: 'To be filled',
+    tab: 'To be filled'
+  })
+}
